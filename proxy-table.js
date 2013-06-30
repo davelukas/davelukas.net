893e32da69f68caaefe9a23a50bdf60265773009
Dave Lukas <dave@davelukas.net>
2013-06-29 23:57:53 -0500

    fix proxy table

--- proxy-table.js
+++ proxy-table.js
@@ -1,12 +1,12 @@
-var until = require('util')
-   , http = require('http')
-   , httpProxy = require('http-proxy')
+var util = require('util')
+  , http = require('http')
+  , httpProxy = require('http-proxy');
 
 httpProxy.createServer({
-	router: {
-	    'localhost': 'localhose:8080',
-	    'www.davelukas.net': 'localhost:7777'
-	}
+    router: {
+        'localhost': 'localhost:8080',
+        'www.davelukas.net': 'localhost:7777'
+    }
 }).listen(80);
 
-util.puts('http proxy server started on port 80');
+util.puts('http proxy server started on port: 80');
