343880d9c748ae4ace702a21aa0155b703f5ac2e
Dave Lukas <dave@davelukas.net>
2013-11-21 13:31:48 -0600

    change app.js to port 80

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 80);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.locals.pretty = true;
